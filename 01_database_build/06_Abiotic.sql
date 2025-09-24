d6f2db764b90be52997b43b54fd05b07b2ad10e8
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2025-09-24 15:28:50 -0800

    feat(database): Add constraints for abiotic elements
    Adds a check constraint to ensure that all abiotic_element_code values are valid and have an element_type of either 'abiotic' or 'both' in the ground_element table.
    
    This prevents incorrect data from being inserted and ensures data integrity.

--- 01_database_build/06_Abiotic.sql
+++ 01_database_build/06_Abiotic.sql
@@ -2,7 +2,7 @@
 -- ---------------------------------------------------------------------------
 -- Build abiotic tables
 -- Author: Timm Nawrocki, Alaska Center for Conservation Science
--- Last Updated: 2025-08-15
+-- Last Updated: 2025-09-24
 -- Usage: Script should be executed in a PostgreSQL 17+ database.
 -- Description: "Build abiotic tables" creates the empty tables for the abiotic components of the AKVEG database. WARNING: THIS SCRIPT WILL ERASE ALL DATA IN EXISTING ABIOTIC TABLES.
 -- ---------------------------------------------------------------------------
@@ -15,13 +15,27 @@ DROP TABLE IF EXISTS
     abiotic_top_cover,
     ground_cover;
 
+-- Create function for constraining abiotic element type
+CREATE OR REPLACE FUNCTION check_abiotic_element(p_code varchar)
+RETURNS boolean AS $$
+BEGIN
+    RETURN EXISTS (
+        SELECT 1 FROM ground_element
+        WHERE ground_element_code = p_code
+          AND element_type IN ('abiotic', 'both')
+    );
+END;
+$$ LANGUAGE plpgsql;
+
 -- Create abiotic top cover table
 CREATE TABLE abiotic_top_cover (
     abiotic_cover_id serial PRIMARY KEY,
     site_visit_code varchar(65) NOT NULL REFERENCES site_visit,
-    abiotic_element_code varchar(2) NOT NULL REFERENCES ground_element,
+    abiotic_element_code varchar(2) NOT NULL,
     abiotic_top_cover_percent decimal(6,3) NOT NULL CONSTRAINT abiotic_percent_range CHECK (abiotic_top_cover_percent BETWEEN 0 AND 100),
-    UNIQUE(site_visit_code, abiotic_element_code)
+    UNIQUE(site_visit_code, abiotic_element_code),
+    CONSTRAINT fk_abiotic_element_code FOREIGN KEY (abiotic_element_code) REFERENCES ground_element(ground_element_code),
+    CONSTRAINT check_abiotic_element_type CHECK (check_abiotic_element(abiotic_element_code))
 );
 
 -- Create ground cover table
