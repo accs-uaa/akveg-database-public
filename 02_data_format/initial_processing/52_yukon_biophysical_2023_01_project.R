534abeeeeb82417794a5ba89aca9999beaca9d4e
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2025-09-29 19:09:07 -0800

    Update 52_yukon_biophysical_2023_01_project.R
    Make project description more concise
    Correct project name

--- 02_data_format/initial_processing/52_yukon_biophysical_2023_01_project.R
+++ 02_data_format/initial_processing/52_yukon_biophysical_2023_01_project.R
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------------
-# "Format Project Table for Yukon Biophysical Inventory Data System Plots"
+# "Format Project Table for Yukon Biophysical Inventory System Plots"
 # Author: Amanda Droghini, Alaska Center for Conservation Science
-# Last Updated: 2025-06-11
+# Last Updated: 2025-09-25
 # Usage: Must be executed in R version 4.4.3+.
-# Description: "Format Project Table for Yukon Biophysical Inventory Data System Plots" enters and formats project-level information for ingestion into the AKVEG Database. The output is a CSV table that can be converted and included in a SQL INSERT statement.
+# Description: "Format Project Table for Yukon Biophysical Inventory System Plots" enters and formats project-level information for ingestion into the AKVEG Database. The output is a CSV table that can be converted and included in a SQL INSERT statement.
 # ---------------------------------------------------------------------------
 
 # Import libraries ----
@@ -36,19 +36,17 @@ template = read_xlsx(path=template_input, col_types = "text")
 # Populate fields ----
 project_data = template %>% 
   add_row(project_code = 'yukon_biophysical_2023') %>% 
-  mutate(project_name = 'Yukon Biophysical Inventory Data System Plots') %>% 
+  mutate(project_name = 'Yukon Biophysical Inventory System Plots') %>% 
   mutate(originator = 'YG') %>%
   mutate(funder = 'YG') %>% 
   mutate(manager = 'Anthony Francis') %>% 
   mutate(completion = 'ongoing') %>% 
   mutate(year_start = as.numeric("1975")) %>% 
   mutate(year_end = as.numeric("-999")) %>% 
-  mutate(project_description = 'Site, soil, and vegetation information collected by multiple agencies to support vegetation inventory, wildlife habitat assessment, and baseline ecosystem products.') %>% 
+  mutate(project_description = 'Site, soil, and vegetation information collected to support vegetation inventory, wildlife habitat assessment, and baseline ecosystem products in Yukon, Canada.') %>% 
   mutate(private = "FALSE")
 
-# QA/QC -----
-
-# Do any of the columns have null values that need to be addressed?
+# Ensure all columns have been filled
 cbind(
   lapply(
     lapply(project_data, is.na)
