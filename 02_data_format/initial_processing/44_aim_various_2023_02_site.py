33dd54b251be97fdd53b9565bc97855c24ad776c
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2025-10-29 15:27:24 -0800

    Update 44_aim_various_2023_02_site.py
    Complete converting R script to Python script

--- 02_data_format/initial_processing/44_aim_various_2023_02_site.py
+++ 02_data_format/initial_processing/44_aim_various_2023_02_site.py
@@ -7,7 +7,16 @@
 # Description: "Format Site Table for BLM AIM Various 2023 data" formats site-level information for ingestion into
 # the AKVEG Database. The script depends on the output from the 44_aim_various_2023_00_extract_data.py script. The
 # script standardizes project and site codes, checks for spatial outliers, formats plot dimension values,
-# and adds required metadata fields. The output is a CSV table that can be converted and included in a SQL INSERT statement.
+# and adds required metadata fields. The output is a CSV table that can be converted and included in a SQL INSERT
+# statement.
+# Notes: For plot dimensions, page 24 of the BLM AIM Wetland Field Protocols states, "The spoke layout is intended for
+# riparian or wetland areas (or zones of interest) that are large enough to accommodate a 30-m radius circle". We
+# assumed all plots that used a spoke layout had a plot dimension of 30 m radius. For transverse plot, we calculated
+# the circular radius using the Plot Area with Minimum Plot Length in Table 4 (Page 29) that corresponded to the
+# ActualPlotLength column (which we assumed was the same as the Maximum Plot Length column in Table 4).
+# Bureau of Land Management. 2024. AIM National Aquatic Monitoring Framework: Field Protocol for Lentic Riparian and
+# Wetland Systems. Tech Reference 1735-3. U.S. Department of the Interior, Bureau of Land Management, National
+# Operations Center, Denver, CO.
 # ---------------------------------------------------------------------------
 
 # Import packages
@@ -34,100 +43,92 @@ template_input = template_folder / '02_site.xlsx'
 site_output = plot_folder / '02_site_aimvarious2023.csv'
 
 # Read in data
-site_original = pl.read_csv(site_input, try_parse_dates=True)
+site_original = pl.read_csv(site_input, try_parse_dates=True, columns=["Project", "PlotID", "EstablishmentDate",
+                                                                       "GPSAccuracy", "SamplingApproach",
+                                                                       "AvgWidthArea", "PlotLayout",
+                                                                       "ActualPlotLength",
+                                                                       "POINT_X", "POINT_Y"])
 project_original = pl.read_csv(project_input)
 template = pl.read_excel(template_input)
 
-# Drop any sites with null coordinates or date (n=0)
-site = site_original.drop_nulls(subset=pl.col("POINT_X", "POINT_Y", "EstablishmentDate"))
-site = site.drop_nans(subset=pl.col("POINT_X", "POINT_Y"))
-
-# Format project code
-site = site.with_columns(
-    pl.when(pl.col("Project") == "UnspecifiedBLM")
-    .then(pl.lit("AK_CentralYukonFO_2022"))
-    .otherwise(pl.col("Project"))
-    .alias("project_name"))
-
-site = site.with_columns(pl.col("project_name").str.replace_many(["AK", "-"], ["AIM", "_"]))
-site = site.with_columns(pl.col("project_name")
-                         .str.replace_all(r"([a-z])([A-Z])", r"${1}_${2}", literal=False)
-                         .str.to_lowercase()
-                         .alias("project_code")
-                         )
 # Ensure site code prefixes are consistent
-site = site.with_columns(pl.col("PlotID").str.extract(pattern=r"(^[a-zA-Z]*-[a-zA-z]*)").alias("site_prefix"))
+site = site_original.with_columns(pl.col("PlotID")
+                                  .str.extract(pattern=r"(^[a-zA-Z]*-[a-zA-z]*)")
+                                  .alias("site_prefix"))
 print(site['site_prefix'].unique())
 
-# Rename site code
-site = site.rename({"PlotID": "site_code"})
+# Drop any sites with null coordinates or date (n=0)
+site = site.drop_nulls(subset=pl.col("POINT_X", "POINT_Y", "EstablishmentDate"))
+site = site.drop_nans(subset=pl.col("POINT_X", "POINT_Y"))
 
 # Filter sites that aren't in Alaska
 ## Coordinates were already re-projected to NAD83 in previous script
 site_filtered = filter_sites_in_alaska(site, input_crs="EPSG:4269", latitude_col="POINT_Y", longitude_col="POINT_X")
 
-# Format plot dimensions
-## Page 24 of the BLM AIM Wetland Field Protocols: "The spoke layout is intended for riparian or wetland areas (or
-# zones of interest) that are large enough to accommodate a 30-m radius circle". Assume that a spoke layout would not
-# have been used if the area was too small for a 30 m radius, even for sites that list an AvgWidthArea less than 60.
-# There is nothing in the comments section for these plots that indicate that a different plot layout or dimension was used.
-# For transverse plot, we calculate the radius of a circle given the Plot Area and Minimum Plot Length listed in
-# Table 4 (Page 29) that corresponds with the value in the ActualPlotLength column.
-# Bureau of Land Management. 2024. AIM National Aquatic Monitoring Framework: Field Protocol for Lentic Riparian and Wetland Systems. Tech Reference 1735-3. U.S. Department of the Interior, Bureau of Land Management, National Operations Center, Denver, CO.
-site_filtered = (site_filtered.with_columns(pl.when(pl.col("PlotLayout") == "Spoke")
-                                           .then(pl.lit("30 radius"))
-                                           .when((pl.col("PlotLayout") == "Transverse") & (pl.col("ActualPlotLength") ==
-                                                 86))
-                                            .then(pl.lit("20 radius"))
-                                            .when((pl.col("PlotLayout") == "Transverse") & (pl.col("ActualPlotLength") ==
-                                                 120))
-                                            .then(pl.lit("15 radius"))
-                                            .otherwise(pl.lit("unknown"))
-                                            .alias("plot_dimensions_m")))
+# Initialize lazy frame
+lazy_site = (
+    site_filtered.lazy()
 
-## Examine sites with unknown plot dimensions
-print(site_filtered.filter(pl.col("plot_dimensions_m") == "unknown"))  # None
+    # Format project code
+    .with_columns(
+        pl.when(pl.col("Project") == "UnspecifiedBLM")
+        .then(pl.lit("AK_CentralYukonFO_2022"))
+        .otherwise(pl.col("Project"))
+        .alias("project_name"))
+    .with_columns(pl.col("project_name").str.replace_many(["AK", "-"], ["AIM", "_"]))
+    .with_columns(pl.col("project_name")
+                  .str.replace_all(r"([a-z])([A-Z])", r"${1}_${2}", literal=False)
+                  .str.to_lowercase()
+                  .alias("establishing_project_code")
+                  )
+    # Rename PlotID columns to site code
+    .rename({"PlotID": "site_code"})
 
-# Populate remaining columns ----
-# Use h_error_m field to inform positional accuracy
-site_data_final = site_data % > %
-mutate(perspective="ground",
-       cover_method="line-point intercept",
-       h_datum="NAD83",
-       longitude_dd=round(POINT_X, digits=5),
-       latitude_dd=round(POINT_Y, digits=5),
-       h_error_m=round(GPSAccuracy, digits=2),
-       positional_accuracy=case_when(h_error_m < 2
-~ "mapping grade GPS",
-.default = "consumer grade GPS"),
-location_type = str_to_lower(SamplingApproach)) % > %
-select(all_of(template))
+    # Format plot dimensions (see Notes in script header)
+    .with_columns(pl.when(pl.col("PlotLayout") == "Spoke")
+                  .then(pl.lit("30 radius"))
+                  .when((pl.col("PlotLayout") == "Transverse") & (pl.col("ActualPlotLength") ==
+                                                                  86))
+                  .then(pl.lit("20 radius"))
+                  .when((pl.col("PlotLayout") == "Transverse") & (pl.col("ActualPlotLength") ==
+                                                                  120))
+                  .then(pl.lit("15 radius"))
+                  .otherwise(pl.lit("unknown"))
+                  .alias("plot_dimensions_m"))
 
-# QA/QC -----
+    # Populate remaining columns
+    .with_columns(pl.lit("ground").alias("perspective"),
+                  pl.lit("line-point intercept").alias("cover_method"),
+                  pl.lit("NAD83").alias("h_datum"),
+                  pl.col("SamplingApproach").str.to_lowercase().alias("location_type"),
+                  pl.when(pl.col("GPSAccuracy").is_not_null())
+                  .then(pl.col("GPSAccuracy").round(decimals=2))
+                  .otherwise(-999)
+                  .alias("h_error_m"))
+    .with_columns(pl.when(pl.col("h_error_m") < 2)
+                  .then(pl.lit("mapping grade GPS"))
+                  .otherwise(pl.lit("consumer grade GPS"))
+                  .alias("positional_accuracy"))
+)
 
-# Do any of the columns have null values that need to be addressed?
-cbind(
-    lapply(
-        lapply(site_data_final, is.na)
-, sum))
+# Execute lazy frame operations
+site_final = lazy_site.collect()
 
-# Check that project codes match with those listed in the 01- Project table
-unique(site_data_final$establishing_project_code) == project_original$project_code
+# Match template columns
+site_final = site_final[template.columns]
 
-# Ensure that site codes are still unique
-length(unique(site_data$site_code)) == nrow(site_data)
-# Check plot dimensions values
-table(site_data_final$plot_dimensions_m)
+# QC
+with pl.Config(tbl_cols=12):
+   print(site_final.describe())
 
-# Check that range of error values is reasonable
-summary(site_data_final$h_error_m)
+## Ensure that project codes match with those listed in the Project table
+print(site_final["establishing_project_code"].unique().sort().equals(project_original["project_code"].sort()))
 
-# Check values of positional accuracy
-site_data_final % > %filter(h_error_m >= 2) % > %nrow()
-table(site_data_final$positional_accuracy)  # 6 sites should be listed as 'consumer grade'
+## Ensure that all site codes are unique (false indicates all codes are unique)
+print(site_final['site_code'].is_duplicated().value_counts())
 
-# Export as CSV ----
-write_csv(site_data_final, site_output)
+## Verify values
+print(site_final["plot_dimensions_m"].value_counts())  # None with 'unknown'
 
-# Clear workspace ----
-rm(list=ls())
+# Export as CSV
+site_final.write_csv(site_output)
