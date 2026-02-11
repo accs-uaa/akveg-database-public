692f2fa1f012753bed858fb3a227359ae7ef2f38
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-02-11 13:17:49 -0900

    Complete processing script

--- taxonomy/taxonomy_updates_20260210.py
+++ taxonomy/taxonomy_updates_20260210.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------------
 # Add Yukon taxa to taxonomy table
 # Author: Amanda Droghini
-# Last Updated: 2026-02-10
+# Last Updated: 2026-02-11
 # Usage: Must be executed in a Python 3.13+ distribution.
 # Description: "Add Yukon taxa to taxonomy table" appends taxa to be added to the taxonomy table and creates unique
 # taxon short codes for the new taxa. The output is a .xlsx table that can be ingested in the AKVEG Database.
@@ -10,38 +10,45 @@
 
 # Import libraries
 import polars as pl
+import taxonomy_utils as tx
 from pathlib import Path
-from taxonomy_utils import generate_taxon_codes
 
 # Define directories
 drive = Path('C:/')
 root = drive / 'ACCS_Work'
-project_folder = root / 'OneDrive - University of Alaska' /'ACCS_Teams' /'Vegetation' / 'AKVEG_Database' / 'Data'
-taxonomy_folder = project_folder / 'Tables_Taxonomy'
+project_folder = root / 'OneDrive - University of Alaska' /'ACCS_Teams' /'Vegetation' / 'AKVEG_Database'
+updates_folder = project_folder / 'Taxonomy_Updates' / '20260211_add_yukon_taxa'
+output_folder = project_folder / 'Data' / 'Tables_Taxonomy'
 
 # Define input
-taxonomy_input = taxonomy_folder / 'taxonomy_20260126.xlsx'
+taxonomy_input = updates_folder / 'taxonomy_20260201.xlsx'
 
 # Define output
-taxonomy_output = taxonomy_folder / 'taxonomy.xlsx'
+taxonomy_output = output_folder / 'taxonomy.xlsx'
 
 # Read in data
-taxonomy_original = pl.read_excel(taxonomy_input, sheet_name='taxonomy')
-taxonomy_yukon = pl.read_excel(taxonomy_input, sheet_name='yukon_to_add',
+taxonomy_original = pl.read_excel(taxonomy_input,
+                                  sheet_name='taxonomy',
+                                  schema_overrides={'code_manual': pl.Int32}
+                                  )
+taxonomy_yukon = pl.read_excel(taxonomy_input,
+                               sheet_name='yukon_to_add',
                                schema_overrides={'taxon_code': pl.String,
-                                                 'code_manual': pl.Boolean}
+                                                 'code_manual': pl.Int32}
                                )
 
-# Verify that there are no null values except for `taxon_code` and `code_manual`
-with pl.Config() as cfg:
-    cfg.set_tbl_cols(16)
-    print(taxonomy_yukon.null_count())
-
 # Obtain taxon short code
-taxonomy_codes = generate_taxon_codes(taxonomy_yukon)
+## Function raises an error if there is a null value in the `taxon_name` column
+yukon_codes = tx.generate_taxon_codes(taxonomy_yukon)
 
-# Ensure no nulls
+# Append Yukon taxa to existing taxonomy table
+taxonomy_original.extend(yukon_codes)
 
-# Ensure no duplicates
+# Correct duplicate codes
+taxonomy_unique = tx.fix_duplicate_codes(taxonomy_original)
 
-# Replace weird whitespace with true spaces
+# Perform final clean-up + null check
+taxonomy_final = tx.final_cleanup(taxonomy_unique)
+
+# Export as .xlsx
+taxonomy_final.write_excel(taxonomy_output, worksheet="taxonomy")
